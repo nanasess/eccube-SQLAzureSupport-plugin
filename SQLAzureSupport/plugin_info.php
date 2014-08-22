c192a2dd0f6127f432e547c733e4d3bf9651cce2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2014-08-22 16:58:35 +0900

    Bump version.

--- SQLAzureSupport/plugin_info.php
+++ SQLAzureSupport/plugin_info.php
@@ -10,15 +10,15 @@ class plugin_info{
     /** プラグインコード(必須)：プラグインを識別する為キーで、他のプラグインと重複しない一意な値である必要があります. */
     static $PLUGIN_CODE       = "SQLAzureSupport";
     /** プラグイン名(必須)：EC-CUBE上で表示されるプラグイン名. */
-    static $PLUGIN_NAME       = "Microsoft Windows Azure プラグイン";
+    static $PLUGIN_NAME       = "Microsoft SQL Database プラグイン";
     /** プラグインバージョン(必須)：プラグインのバージョン. */
-    static $PLUGIN_VERSION    = "1.0";
+    static $PLUGIN_VERSION    = "2.0";
     /** 対応バージョン(必須)：対応するEC-CUBEバージョン. */
-    static $COMPLIANT_VERSION = "2.12.0";
+    static $COMPLIANT_VERSION = "2.13.2";
     /** 作者(必須)：プラグイン作者. */
     static $AUTHOR            = "Kentaro Ohkouchi (Loop AZ)";
     /** 説明(必須)：プラグインの説明. */
-    static $DESCRIPTION       = "Microsoft Windows Azure 対応プラグインです。インストーラのデータベースに SQL Azure が追加されます。PHP 5.3.0 以降対応。sqlsrv ドライバが必要です。";
+    static $DESCRIPTION       = "Microsoft SQL Database 対応プラグインです。インストーラのデータベースに SQL Database が追加されます。PHP 5.3.0 以降対応、PHP5.4.x 推奨です。sqlsrv ドライバが必要です。プラグインを有効化すると、サイトが初期化され、インストール画面に遷移しますのでご注意ください。";
     /** プラグインURL：プラグイン毎に設定出来るURL（説明ページなど） */
     static $PLUGIN_SITE_URL   = "";
     /** プラグイン作者URL：プラグイン毎に設定出来るURL（説明ページなど） */
@@ -30,4 +30,3 @@ class plugin_info{
     /** プラグインのライセンス. */
     static $LICENSE           = "GPL";
 }
-?>
